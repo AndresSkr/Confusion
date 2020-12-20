2484142e9f0e2cd0f6e1c67b7cfbc465bb8b3485
Andres <jhohan.andres83@gmail.com>
2020-12-20 17:54:13 -0500

    Page AboutUs add Course

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, CardText } from 'reactstrap';
+import { Card,CardBody,CardHeader, Media, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function RenderLeaders({ leaders }) {
@@ -7,30 +7,30 @@ function RenderLeaders({ leaders }) {
         console.log(leader);
         return (
             <>
-            <li key={leader.id} className="mb-2">
-                <div className="row">
-                    <div className="col-12 col-md-2">
-                        <img width="100%" src={leader.image} />
-                    </div>
-                    <div className="col-12 col-md-10">
-                        <h4>
-                            {leader.name}
-                        </h4>
-                        <div>
-                            <p>
-                                {leader.designation}
-                            </p>
+                <li key={leader.id} className="mb-2">
+                    <div className="row">
+                        <div className="col-12 col-md-2">
+                            <img width="100%" src={leader.image} />
                         </div>
-                        <div>
-                            <p>
-                                {leader.description}
-                            </p>
+                        <div className="col-12 col-md-10">
+                            <h4>
+                                {leader.name}
+                            </h4>
+                            <div>
+                                <p>
+                                    {leader.designation}
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    {leader.description}
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </li>
-            <hr/>
+                </li>
+                <hr />
             </>
         )
     })
@@ -43,103 +43,66 @@ function RenderLeaders({ leaders }) {
 }
 
 const About = (props) => {
-
     return (
         <div className="container">
-
             <div className="row">
                 <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to="/home">Home</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem>
-                        About Us
-                    </BreadcrumbItem>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>About Us</BreadcrumbItem>
                 </Breadcrumb>
                 <div className="col-12">
-                    <h1>About Us</h1>
+                    <h3>About Us</h3>
                     <hr />
                 </div>
             </div>
-
-            <div className="row">
-
-                <div className="col-8 col-md-6 m-1">
+            <div className="row row-content">
+                <div className="col-12 col-md-6">
                     <h2>Our History</h2>
-                    <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary
-                    icon par excellence in Hong Kong. With its unique brand of world fusion cuisine
-                    that can be found nowhere else, it enjoys patronage from the A-list clientele in
-                    Hong Kong. Featuring four of the best three-star Michelin chefs in the world, you
-                    never know what will arrive on your plate the next time you visit us.
-                    </p>
-                    <p>
-                        The restaurant traces its humble beginnings to The Frying Pan,
-                        a successful chain started by our CEO, Mr. Peter Pan, that featured
-                        for the first time the world's best cuisines in a pan.
-                    </p>
-
+                    <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
+                    <p>The restaurant traces its humble beginnings to <em>The Frying Pan</em>, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
                 </div>
-
-                <div className="col-12 col-md-4 m-1">
+                <div className="col-12 col-md-5">
                     <Card>
-                        <CardTitle className="bg-primary text-white p-2 " center>
-                            Facts At a Glance
-                            </CardTitle>
-                        <CardText className="p-2">
-                            <div className="row">
-                                <div className="col-6">
-                                    <strong><p>Started</p></strong>
-
-                                </div>
-                                <div className="col-6">
-                                    <p>Feb, 2013</p>
-                                </div>
-
-                                <div className="col-6">
-                                    <strong> <p>Major Stake Holder </p></strong>
-
-                                </div>
-                                <div className="col-6">
-                                    <p>HK Fine Foods Inc.</p>
-                                </div>
-
-                                <div className="col-6">
-                                    <strong><p>Last Year's Turnover </p></strong>
-                                </div>
-                                <div className="col-6">
-                                    <p>$1,250,375</p>
-                                </div>
-
-                                <div className="col-6">
-                                    <strong>
-                                        <p>Employeed</p>
-                                    </strong>
-                                </div>
-                                <div className="col-6">
-                                    <p>40</p>
-                                </div>
-                            </div>
-                        </CardText>
+                        <CardHeader className="bg-primary text-white">Facts At a Glance</CardHeader>
+                        <CardBody>
+                            <dl className="row p-1">
+                                <dt className="col-6">Started</dt>
+                                <dd className="col-6">3 Feb. 2013</dd>
+                                <dt className="col-6">Major Stake Holder</dt>
+                                <dd className="col-6">HK Fine Foods Inc.</dd>
+                                <dt className="col-6">Last Year's Turnover</dt>
+                                <dd className="col-6">$1,250,375</dd>
+                                <dt className="col-6">Employees</dt>
+                                <dd className="col-6">40</dd>
+                            </dl>
+                        </CardBody>
                     </Card>
                 </div>
-
-                <blockquote className="blockquote mb-0 card-body">
-                    <p>You better cut the pizza in four pieces because I'm not hungry enough to eat six.</p>
-                    <footer className="blockquote-footer">
-                        <small className="text-muted">
-                        Yogi Berra, <cite title="Source Title">The Wit and Wisdom of Yogi Berra, P. Pepe, Diversion Books, 2014</cite>
-                        </small>
-                    </footer>
-                </blockquote>
+                <div className="col-12">
+                    <Card>
+                        <CardBody className="bg-faded">
+                            <blockquote className="blockquote">
+                                <p className="mb-0">You better cut the pizza in four pieces because
+                            I'm not hungry enough to eat six.</p>
+                                <footer className="blockquote-footer">Yogi Berra,
+                        <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
+                            P. Pepe, Diversion Books, 2014</cite>
+                                </footer>
+                            </blockquote>
+                        </CardBody>
+                    </Card>
+                </div>
             </div>
-
-            <hr />
-            <div className="container">
-                <ul style={{ listStyleType: "none" }} >
-                    <RenderLeaders leaders={props.leaders} />
-                </ul>
+            <div className="row row-content">
+                <div className="col-12">
+                    <h2>Corporate Leadership</h2>
+                </div>
+                <div className="col-12">
+                    <Media style={{ listStyleType: "none" }} list>
+                       <RenderLeaders leaders={props.leaders}/>
+                    </Media>
+                </div>
             </div>
-
         </div>
     );
 }
